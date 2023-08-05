2028a2d0eaf94a3cdf1f47029e3b1af4e2f9d01e
enrique <enriquegut97@gmail.com>
2023-08-05 00:28:09 -0700

    pagination added to home page

--- components/QuestionContainer.tsx
+++ components/QuestionContainer.tsx
@@ -4,15 +4,42 @@ import { IQuestion } from '@/interfaces';
 import { getAllQuestions } from '@/utils/services';
 import Question from './Question';
 import '@/styles/QuestionContainer.scss';
+import Pagination from './Pagination';
 
 const QuestionContainer = () => {
 
     const [questions, setQuestions] = useState<IQuestion[]>([]);
-
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    
     useEffect(()=>{
         getAllQuestions().then((res)=>{setQuestions(res?.data)})
         
     },[])
+
+    //Pagination
+    const itemsLength = questions.length;
+    const maxItems: number = 10;
+    const paginationLength = Math.ceil(itemsLength / maxItems)
+    const maxRange = (currentPage * maxItems ) - 1;
+    const minRange = (maxRange - maxItems ) + 1;
+    const questionsInPage = questions.slice(minRange,maxRange + 1);
+
+    const previousPage = ()=>{
+        if(currentPage > 1){
+            setCurrentPage((page)=>page - 1);
+        }
+    };
+
+    const nextPage = ()=>{
+        if(currentPage < paginationLength){
+            setCurrentPage((page)=>page + 1);
+        }
+    };
+
+    const setPage = (page:number)=>{
+        setCurrentPage(page);
+    };
+
 
          
 
@@ -20,7 +47,7 @@ const QuestionContainer = () => {
 
   return (
     <div className='container questions-c'>
-        {questions.map((q,i)=>{
+        {questionsInPage.map((q,i)=>{
             return (
                 <Question
                 name={q.creator.name}
@@ -30,6 +57,12 @@ const QuestionContainer = () => {
                 key={i} />
             )
         })}
+        <Pagination
+        paginationLength={paginationLength}
+        currentPage={currentPage}
+        nextPage={nextPage}
+        previousPage={previousPage}
+        setPage={setPage} />
 
     </div>
   )
